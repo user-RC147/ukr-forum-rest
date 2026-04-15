0de69b1132381bce9c4be2ce0825815d54551ea3
Viktor <viktor.ustenko.de@gmail.com>
2026-04-15 12:22:37 +0200

    стартовий проект початок є тільки юзер, модель з классом але порошнім

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'apps.users.CustomUser'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
