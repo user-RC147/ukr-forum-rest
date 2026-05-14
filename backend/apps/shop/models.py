425c8cc20581969a24ac50b0092b163984ce99f3
evilsenpa1 <alexcorporationo@gmail.com>
2026-05-14 16:39:26 +0200

    feat: create model for products, base functional and meta

--- backend/apps/shop/models.py
+++ backend/apps/shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ProductModel(models.Model):
+    title = models.CharField(max_length=300, blank=False)
+    description = models.CharField(max_length=1500)
+    price = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # owner = models.ForeignKey("users.User", on_delete=models.CASCADE, db_column="owner_id")
+
+    class Meta:
+        db_table = "product"
+        ordering = ["-id"]
+        verbose_name = "Товар"
+        verbose_name_plural = "Товари"
+
+    def __str__(self):
+        if self.updated_at != self.created_at:
+            return f"(upd: {self.updated_at:%d.%m.%Y}){self.title} - {self.price} грн."
+        return f"(created: {self.created_at:%d.%m.%Y}){self.title} - {self.price} грн."
