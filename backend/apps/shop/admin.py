74e537e7093dffb20c90353987b43ba62eefeff2
evilsenpa1 <alexcorporationo@gmail.com>
2026-05-14 16:43:52 +0200

    feat: register model in admin panel + some admin features

--- backend/apps/shop/admin.py
+++ backend/apps/shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ProductModel
 
-# Register your models here.
+@admin.register(ProductModel)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'description']
+    list_per_page = 25
+    date_hierarchy = 'created_at'
+    ordering = ['-created_at']
+    show_full_result_count = False   
