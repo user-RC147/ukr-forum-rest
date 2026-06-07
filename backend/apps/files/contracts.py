c37ba92813327aedd9e6eef54c95ce36b85a741f
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-07 14:51:02 +0200

    feat: add new methods to contract layer, rename some methods and fields

--- backend/apps/files/contracts.py
+++ backend/apps/files/contracts.py
@@ -1,8 +1,8 @@
 from django.core.files.uploadedfile import UploadedFile
-from files.protocols import FileProtocol
-from files.services import FileService
 
 from apps.files.dto import FileDTO
+from apps.files.protocols import FileProtocol
+from apps.files.services import FileService
 
 
 class FileContract:
@@ -15,9 +15,20 @@ class FileContract:
     def get_file(self, file_id: int) -> FileDTO:
         return self.service.get_file(file_id)
 
-    def file_delete(self, file_id: int, user_id: int) -> bool:
-        self.service.delete_file(file_id, user_id)
-        return True
+    def get_files_map(self, files_ids: list[int]) -> dict[int, FileDTO]:
+        return self.service.get_files_map(files_ids)
+
+    def create_files_list(
+        self, data: list[UploadedFile], user_id: int
+    ) -> list[FileDTO]:
+        return self.service.create_files_list(data, user_id)
+
+    def delete_file(self, file_id: int) -> None:
+
+        return self.service.delete_file(file_id)
+
+    def delete_files_map(self, files_ids: list[int]) -> None:
+        return self.service.delete_files_map(files_ids)
 
 
 def get_file_contract() -> FileProtocol:
--- backend/apps/files/dto.py
+++ backend/apps/files/dto.py
@@ -1,13 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from django.db.models import FileField
-
 
 @dataclass(frozen=True)
 class FileDTO:
     owner_id: int
     file_id: int
-    file: FileField
-    vizible: bool
+    file: str
+    visible: bool
     created_at: datetime
