038e5970a86bb481f54477375fac39b0dd914bb1
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-13 19:23:04 +0200

    feat: create register of search handlers from others modules

--- backend/apps/search/apps.py
+++ backend/apps/search/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class SearchConfig(AppConfig):
-    name = 'apps.search'
-    verbose_name = 'Пошук'
+    name = "apps.search"
+    verbose_name = "Пошук"
+
+    def ready(self) -> None:
+        from .registry import SearchRegistry
+
+        SearchRegistry.load_from_settings()
