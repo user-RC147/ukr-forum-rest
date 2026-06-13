c3bf7e71e7a80f02cf345086b608fb91677b4377
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-13 22:12:38 +0200

    feat: create data formatting in serializer

--- backend/apps/shop/serializers.py
+++ backend/apps/shop/serializers.py
@@ -8,7 +8,12 @@ class ProductSerializer(serializers.Serializer):
     country_id = serializers.IntegerField()
     region_id = serializers.IntegerField()
     city_id = serializers.IntegerField()
-    files = serializers.ListField(child=serializers.ImageField(allow_empty_file=False), allow_empty=False, max_length=10,)
+    files = serializers.ListField(
+        child=serializers.ImageField(allow_empty_file=False),
+        allow_empty=False,
+        max_length=10,
+    )
+
 
 class ProductReadSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -16,7 +21,7 @@ class ProductReadSerializer(serializers.Serializer):
     title = serializers.CharField()
     description = serializers.CharField()
     price = serializers.IntegerField()
-    created_at = serializers.DateTimeField()
+    created_at = serializers.DateTimeField(format="%d.%m.%Y %H:%M:%S")
     country_id = serializers.IntegerField()
     region_id = serializers.IntegerField()
     city_id = serializers.IntegerField()
