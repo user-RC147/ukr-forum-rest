c997d85cbe8ef7f46457afef3838fd5c6b3472cd
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-20 20:53:17 +0200

    refactor: minor fixes

--- frontend/src/modules/shop/components/FloatingActions.vue
+++ frontend/src/modules/shop/components/FloatingActions.vue
@@ -40,6 +40,9 @@ onUnmounted(() => document.removeEventListener('click', onDocClick))
 
     <!-- Тоглер -->
     <button
+      type="button"
+      :aria-expanded="isOpen"
+      aria-label="Інші дії"
       @click.stop="isOpen = !isOpen"
       class="text-sm font-semibold text-white bg-orange-400/80 hover:bg-orange-500/90
              px-5 py-2.5 rounded-xl shadow-md transition-all duration-200 active:scale-95"
