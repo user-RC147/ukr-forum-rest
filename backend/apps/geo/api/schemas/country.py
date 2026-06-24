8ea0d690e75436acbb8e28d836ce7b637d90bbdf
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-24 14:56:36 +0200

    feat: create apis for geo search (views, serializers)

---
+++ backend/apps/geo/api/schemas/country.py
@@ -0,0 +1,21 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    OpenApiParameter,
+    extend_schema,
+)
+
+from apps.geo.api.serializers.resolve import CountrySerializer
+
+country_search_schema = extend_schema(
+    summary="Результати пошуку країн",
+    parameters=[
+        OpenApiParameter(
+            name="q",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            description="Пошуковий запит",
+            required=False,
+        ),
+    ],
+    responses={200: CountrySerializer(many=True)},
+)
--- backend/apps/geo/api/views/cities.py
+++ backend/apps/geo/api/views/cities.py
@@ -2,10 +2,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.request import Request
 from django.apps import apps
 from dataclasses import asdict
-
+from apps.geo.api.serializers.resolve import CitySerializer
+from apps.geo.api.schemas.city import city_search_schema
 
 class CityListView(APIView):
     """
@@ -24,3 +25,19 @@ class CityListView(APIView):
             return Response({'results': []})
         cities = geo_service.fetch_and_save_cities(int(region_id))
         return Response({'results': [asdict(c) for c in cities]})
+    
+class CitySearchView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CitySerializer
+
+
+    @city_search_schema
+    def get(self, request: Request):
+        query = request.query_params.get("q", "")
+        country_id = request.query_params.get("country_id", "")
+        if len(query) < 2 or not country_id:
+            return Response({"results": [], "query": query})
+
+        geo_service = apps.get_app_config('geo').service
+        serializer = CitySerializer(geo_service.search_cities(query, country_id), many=True)
+        return Response(serializer.data)
