336221194c8e6d46d05390025a2d662833008bac
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-27 21:45:33 +0200

    feat: create category and tag models

--- backend/apps/search/admin.py
+++ backend/apps/search/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import CategoryModel, TagModel
+
 # Register your models here.
+
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TagModel)
+class TagModelAdmin(admin.ModelAdmin):
+    pass
