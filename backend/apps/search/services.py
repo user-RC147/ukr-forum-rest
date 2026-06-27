839c829b7d0185d9cfd08cb5a93d5b50e0f8fee4
evilsenpa1 <alexcorporationo@gmail.com>
2026-06-27 21:46:55 +0200

    feat: create service methods for categories and tags

--- backend/apps/search/services.py
+++ backend/apps/search/services.py
@@ -1,7 +1,10 @@
 # search/services.py
 import logging
-
-from .contracts import SearchResultItem
+from django.core.exceptions import ObjectDoesNotExist
+from .contracts.protocols import SearchResultItem
+from .dto import CategoryDTO, TagDTO
+from .contracts.exceptions import CategoryNotFoundError, TagNotFoundError
+from .models import CategoryModel, TagModel
 from .registry import SearchRegistry
 
 logger = logging.getLogger(__name__)
@@ -10,6 +13,8 @@ logger = logging.getLogger(__name__)
 class SearchService:
     def __init__(self, limit_per_module: int = 10) -> None:
         self.limit_per_module = limit_per_module
+        self.category_model = CategoryModel
+        self.tag_model = TagModel
 
     def search(self, params, scope: str | None = None) -> list[SearchResultItem]:
         handlers = SearchRegistry.all()
@@ -25,3 +30,35 @@ class SearchService:
                     raise
 
         return results
+
+    def get_categories(self, category_ids: list[int]) -> dict[int, CategoryDTO]:
+        result = self.category_model.objects.filter(id__in=category_ids).prefetch_related("tags")
+        return {r.id: _to_dto_category(r) for r in result}
+
+    def get_category(self, category_id: int):
+        try:
+            result = self.category_model.objects.get(id=category_id)
+        except ObjectDoesNotExist:
+            logger.info("Category with id: %s not found!", category_id)
+            raise CategoryNotFoundError("Category with this id does not exist")
+
+        return _to_dto_category(result)
+    
+    def get_tag(self, tag_id: int) -> TagDTO:
+        try:
+            result = self.tag_model.objects.get(id=tag_id)
+        except ObjectDoesNotExist:
+            logger.info("Tag with id: %s not found!", tag_id)
+            raise TagNotFoundError("Category with this id does not exist")
+
+        return _to_dto_tag(result)
+    
+    def get_tags(self, tag_ids: list[int]) -> dict[int, TagDTO]:
+        result = self.tag_model.objects.filter(id__in=tag_ids)
+        return {r.id: _to_dto_tag(r) for r in result}
+
+def _to_dto_category(data) -> CategoryDTO:
+    return CategoryDTO(id=data.id, name=data.name, tags={t.id: _to_dto_tag(t) for t in data.tags})
+
+def _to_dto_tag(data) -> TagDTO:
+    return TagDTO(id=data.id, name=data.name)
