86050c57b1d7c2a65458b09f150b0aee05139276
evilsenpa1 <alexcorporationo@gmail.com>
2026-07-10 18:26:57 +0200

    chore: create docker for frontend, add proxy in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,33 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),  // ← додати
+    tailwindcss(),
   ],
-  resolve:{
-    alias:{
-      '@':fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+
+    strictPort: true,
+
+    watch: {
+      usePolling: true,
+    },
+
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',
+        changeOrigin: false,
+      },
+    },
+  },
 })
