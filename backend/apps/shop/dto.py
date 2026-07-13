1a23cc70a0237e5cda65dc0f86b0a8aadf65d0f8
evilsenpa1 <alexcorporationo@gmail.com>
2026-07-13 15:19:50 +0200

    refactor(shop): create new dtos

--- backend/apps/shop/dto.py
+++ backend/apps/shop/dto.py
@@ -1,6 +1,59 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+from django.core.files.uploadedfile import UploadedFile
+
+from apps.files.dto import FileDTO
+from apps.geo.dto.city import CityDTO
+from apps.geo.dto.country import CountryDTO
+from apps.geo.dto.region import RegionDTO
+from apps.search.dto import CategoryDTO
+
+
+@dataclass(frozen=True)
+class ProductUpdateDTO:
+    id: int
+    title: str | None = None
+    description: str | None = None
+    country_id: int | None = None
+    region_id: int | None = None
+    city_id: int | None = None
+    category_id: int | None = None
+    price: int | None = None
+    update_file_ids: list[int] | None = None
+    update_files: list[UploadedFile] | None = None
+    create_files: list[UploadedFile] | None = None
+    keep_files_ids: list[int] | None = None
+
+
+@dataclass(frozen=True)
+class ProductCreateDTO:
+    title: str
+    owner_id: int
+    description: str
+    country_id: int
+    region_id: int
+    city_id: int
+    category_id: int
+    price: int
+    files: list[UploadedFile]
+
+
+@dataclass(frozen=True)
+class ProductRepoDTO:
+    id: int
+    owner_id: int
+    title: str
+    description: str
+    created_at: datetime
+    country_id: int
+    region_id: int
+    city_id: int
+    category_id: int
+    price: int
+    visible: bool
+    file_ids: list[int]
+
 
 @dataclass(frozen=True)
 class ProductDTO:
@@ -9,7 +62,10 @@ class ProductDTO:
     title: str
     description: str
     created_at: datetime
-    country_id: int
+    country: CountryDTO
+    region: RegionDTO
+    city: CityDTO
+    category: CategoryDTO
     price: int
-    visible: bool | None
-    files: list | None
+    visible: bool
+    files: dict[int, FileDTO]
