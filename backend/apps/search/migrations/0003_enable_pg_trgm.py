f1644f796639f0b37dac8697d13eefba6a9d630f
evilsenpa1 <alexcorporationo@gmail.com>
2026-07-28 21:44:22 +0200

    refactor(search): move migrations with pg_trigramm install

--- backend/apps/search/migrations/0003_enable_pg_trgm.py
+++ backend/apps/search/migrations/0003_enable_pg_trgm.py
@@ -5,7 +5,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("geo", "0001_initial"),
+        ("search", "0002_alter_tagmodel_category"),
     ]
 
     operations = [
