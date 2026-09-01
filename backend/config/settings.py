5e89b2e6f8029e277f8941ddde14ec6cc0dd3814
evilsenpa1 <alexcorporationo@gmail.com>
2026-09-01 16:55:16 +0200

    feat(docker): add ssl certificate logic to nginx and django
    * fix bug with django_debug_toolbar (set up in settings)
    * improve env.example

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -71,6 +71,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+if DEBUG:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
@@ -196,7 +202,7 @@ if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     if USE_HTTPS:
-        SECURE_SSL_REDIRECT = True
+        SECURE_SSL_REDIRECT = False #fix 301 from healthcheck
         SECURE_HSTS_SECONDS = 31536000
         SECURE_HSTS_INCLUDE_SUBDOMAINS = True
         SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
